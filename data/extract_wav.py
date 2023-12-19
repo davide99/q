8f9a02fedddc3b5707ecc3799201109f14fa216c
dav.pisano <dav.pisano@reply.it>
2023-12-19 14:10:47 +0100

    added A sample

--- data/extract_wav.py
+++ data/extract_wav.py
@@ -1,22 +1,23 @@
 import wave
 
 
-def read_and_print_wav(input_file, output_file):
-    with wave.open(input_file, 'rb') as wav_file:
-        assert wav_file.getnchannels() == 1
-        assert wav_file.getsampwidth() == 2  # bytes
-        assert wav_file.getframerate() == 44100
+def read_and_print_wav(input_files, output_file):
+    with open(output_file, 'w') as output:
+        for input_file in input_files:
+            with wave.open(input_file, 'rb') as wav_file:
+                assert wav_file.getnchannels() == 1
+                assert wav_file.getsampwidth() == 2  # bytes
+                assert wav_file.getframerate() == 44100
 
-        with open(output_file, 'w') as output:
-            for _ in range(wav_file.getnframes()):
-                frame = wav_file.readframes(1)
-                sample = int.from_bytes(frame, byteorder='little', signed=False)
+                for _ in range(wav_file.getnframes()):
+                    frame = wav_file.readframes(1)
+                    sample = int.from_bytes(frame, byteorder='little', signed=False)
 
-                output.write(f"{sample}\n")
+                    output.write(f"{sample}\n")
 
 
 if __name__ == '__main__':
     # Esempio di utilizzo
-    input_wav_file = 'low-e.wav'
+    input_wav_files = ['low-e.wav', 'a.wav']
     output_txt_file = 'samples.csv'
-    read_and_print_wav(input_wav_file, output_txt_file)
+    read_and_print_wav(input_wav_files, output_txt_file)
