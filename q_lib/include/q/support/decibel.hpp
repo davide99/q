a212cb40655cbaa007d465e38204575062ed28f4
davide99 <info.davide99@gmail.com>
2023-12-24 15:28:16 +0100

    cleanup decibel.hpp

--- q_lib/include/q/support/decibel.hpp
+++ q_lib/include/q/support/decibel.hpp
@@ -10,35 +10,6 @@
 #include <q/support/unit.hpp>
 
 namespace cycfi::q {
-    ////////////////////////////////////////////////////////////////////////////
-    // decibel is a highly optimized class for dealing with decibels. The
-    // class provides fast conversion from linear to decibel and back. The
-    // decibel value is non-linear. It operates on the logarithmic domain. The
-    // decibel class is perfectly suitable for dynamics processing (e.g.
-    // compressors and limiters and envelopes). Q provides fast decibel
-    // computations using lookup tables and fast math computations for
-    // converting to and from scalars.
-    ////////////////////////////////////////////////////////////////////////////
-
-    struct decibel : unit<double, decibel> {
-        using base_type = unit<double, decibel>;
-        using base_type::base_type;
-
-        // Please use lin_to_db(s) instead of decibel(s).
-        //
-        decibel(double) = delete;
-        //
-        // This constructor was used in previous versions of the library to
-        // convert linear to decibels, which can be confusing. This version
-        // ought to have corrected this nonintuitive semantics, but this is a
-        // disruptive change that will alter the semantics of all existing code
-        // without warning. In order to avoid further confusion, we will mark
-        // this constructor as deleted for the time being, making it a hard
-        // error to alert users upgrading to this library version.
-    };;
-
-    constexpr float lin_float(float db);
-
     constexpr float lin_float(float db) {
         return powf(10, db / 20.0f);
     }
